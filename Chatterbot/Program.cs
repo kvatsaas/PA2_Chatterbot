ed0844aef897fbc51a446b7ef0ee1f31fda8bd76
vatsa008 <vatsa008@d.umn.edu>
2021-09-24 17:32:16 -0500

    Add project files.

---
+++ Chatterbot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chatterbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
